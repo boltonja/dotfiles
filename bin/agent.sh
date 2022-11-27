ea0216c7075ebadb2c6e749961ecd991e386fbea
Jillian A. Bolton <jillian@rootaction.net>
2022-11-26 20:03:15 -0800

    compatibility with NetBSD, FreeBSD, OpenBSD, Darwin

--- bin/agent.sh
+++ bin/agent.sh
@@ -1,10 +1,11 @@
 host=`hostname -s`
 user=`whoami`
 agfil=~/agent.$host
+os=`uname -s`
 agpid=`grep -s PID $agfil | sed 's/.*PID=\([0-9]*\)\; export.*/\1/'`
 if [ "${agpid}" != "" ]
 then
-	ps -fp ${agpid} | grep "$user.*ssh-agent"
+	ps up ${agpid} | grep "$user.*ssh-agent"
 	if [ $? != 0 ]
 	then
 		echo "new agent"
