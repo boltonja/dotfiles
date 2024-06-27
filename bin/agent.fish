c0935663c6d47ffe5482cb73e95fdc613258a19b
Jillian A. Bolton <jillian@rootaction.net>
2024-06-27 14:36:44 -0600

    simplify agent check in agent.fish to match agent.sh

--- bin/agent.fish
+++ bin/agent.fish
@@ -5,7 +5,7 @@ set agfil ~/agent.$host
 set agpid (grep PID $agfil | sed 's/.*PID=\([0-9]*\)\; export.*/\1/')
 if test -n $agpid
 	echo looking for $agpid
-	ps xu | grep "^$user"'[^0-9][^0-9]*'"$agpid.*ssh-agent"
+	ps up $agpid | grep "^$user.*ssh-agent"
 	if test $status != 0
 		echo "new agent"
 		ssh-agent > $agfil
